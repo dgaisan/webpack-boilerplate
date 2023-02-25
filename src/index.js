8d4c864f42aad2154e0ff9a9307ccb2c97def54d
dgaisan <dima.gaisan@gmail.com>
2023-02-25 00:43:32 -0700

    Adding scss loaders and styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles/main.scss';
 import generateString from "./generateString";
 
 console.log(generateString());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
-}
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
