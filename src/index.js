a4c6fc1306dd96a314df5f2c760b75e0974e022b
dgaisan <dima.gaisan@gmail.com>
2023-02-25 23:20:06 -0700

    adding babel loader, assets loader, and bundle analyzer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import './styles/main.scss';
+import "./styles/main.scss";
 import generateString from "./generateString";
+import svgAsset from "./assets/svgAsset.svg";
+
+const logoElem = document.getElementById("logo");
+logoElem.src = svgAsset;
 
 console.log(generateString());
+console.log(123);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    compress: true,
+    hot: true,
+    open: true,
+    port: 3001,
+    static: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -13,6 +26,28 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/i,
+        type: 'asset'
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Boilerplate",
+      filename: "index.html",
+      template: "src/index.template.html",
+    }),
+    new BundleAnalyzerPlugin({analyzerMode: 'static'}),
+  ],
 };
